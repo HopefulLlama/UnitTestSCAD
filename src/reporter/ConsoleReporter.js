50ef519b0c1af1ba936b8ace93b390401e8da930
HopefulLlama <jonlaw10170@gmail.com>
2017-05-30 19:58:11 +0100

    #15: Added some colours to console reporter

--- src/reporter/ConsoleReporter.js
+++ src/reporter/ConsoleReporter.js
@@ -2,13 +2,26 @@ var os = require('os');
 
 var TAB = '    ';
 
+var TERMINAL = {
+  'GREEN': '\x1b[32m',
+  'RED': '\x1b[31m',
+  'RESET': '\x1b[0m'
+};
+
 var report = function(results) {
   results.testSuites.forEach(function(testSuiteSummary) {
     console.log(testSuiteSummary.name + ': ');
 
     testSuiteSummary.tests.forEach(function(testSummary) {
       console.log(TAB + testSummary.name + ': ');
-      console.log(TAB.repeat(2) + testSummary.failures.length + ' failures in ' + testSummary.assertions + ' assertions.');
+      var summary = TAB.repeat(2);
+      if(testSummary.failures.length === 0) {
+        summary += TERMINAL.GREEN;
+      } else {
+        summary += TERMINAL.RED;
+      }
+      summary += testSummary.failures.length + ' failures' + TERMINAL.RESET + ' in ' + testSummary.assertions + ' assertions.';
+      console.log(summary);
 
       if(testSummary.failures.length > 0) {
         console.log(TAB.repeat(2) + testSummary.failures.join(os.EOL + TAB.repeat(2)));
@@ -17,7 +30,15 @@ var report = function(results) {
   });
 
   console.log();
-  console.log(results.failures + ' total failures in ' +  results.assertions + ' total assertions.'); 
+
+  var summary = '';
+  if(results.failures === 0) {
+    summary += TERMINAL.GREEN;
+  } else {
+    summary += TERMINAL.RED;
+  }
+  summary += results.failures + ' total failures' + TERMINAL.RESET + ' in ' +  results.assertions + ' total assertions.'
+  console.log(summary); 
 };
 
 module.exports = report;

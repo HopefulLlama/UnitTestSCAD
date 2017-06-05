70bef7db8b5cc7207d1b0a5b81a24b75d7083420
HopefulLlama <jonlaw10170@gmail.com>
2017-06-05 23:44:11 +0100

    Added unit test

---
+++ spec/reporter/JsonReporterSpec.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+var JsonReporter = require('../../src/reporter/JsonReporter');
+
+describe('JsonReporter', function() {
+	it('should write a JSON file', function() {
+		var report = {
+			'Yolo': 'Swag',
+			'MoneyMoney': 'Yeah'
+		};
+
+		var options = {
+			'dest': './JsonReporterSpec.json'
+		};
+
+		JsonReporter(report, options);
+
+		if(fs.existsSync(options.dest)) {
+			var contents = fs.readFileSync(options.dest, 'utf8');
+			expect(report).toEqual(JSON.parse(contents));
+
+			fs.unlink(options.dest);
+		} else {
+			fail('File not written.');
+		}
+	});
+});

575438297044182b5e65d79504032c5909f82747
Jonathan Law <jonlaw10170@gmail.com>
2017-06-28 21:57:23 +0100

    Remove trailing comma

--- src/util/ErrorHandler.js
+++ src/util/ErrorHandler.js
@@ -7,7 +7,7 @@ function ErrorHandler() {}
 ErrorHandler.prototype.REASONS = {
   ASSERTION_FAILURES: 'Exiting with error, due to failing test assertions.',
   INVALID_CONFIG: USAGE + os.EOL + 'The supplied path to the configuration file does not point to a valid configuration file.',
-  MISSING_CONFIG: USAGE + os.EOL + 'Must specify a configuration file when calling UnitTestSCAD.',
+  MISSING_CONFIG: USAGE + os.EOL + 'Must specify a configuration file when calling UnitTestSCAD.'
 };
 
 ErrorHandler.prototype.throwErrorAndSetExitCode = function(reason) {

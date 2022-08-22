f9865d9f0ba9a1a80d71c980763c813433364403
Georg <geg@have2.com>
2022-08-22 22:39:32 +0200

    Update TwoDModuleFile.js
    Part 4 of set variables patch

--- src/file/TwoDModuleFile.js
+++ src/file/TwoDModuleFile.js
@@ -3,11 +3,12 @@ const File = require('./File');
 const svgFile = 'UnitTestSCAD_48967_TEMP_DELETE-ME_TWO_D.svg';
 
 module.exports = {
-  execute(header, setUpText, testText) {
+  execute(header, setUpText, testText, setVariables) {
     return File.execute({
       header,
       setUpText,
       testText,
+      setVariables,
     }, svgFile).file;
   }
 };
